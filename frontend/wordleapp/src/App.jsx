f083605bd883d6a925c7c3a9613508b66c8aee0b
MysticalFlame <edwardnliu@gmail.com>
2025-06-17 18:23:17 -0500

    squares
    yay

--- frontend/wordleapp/src/App.jsx
+++ frontend/wordleapp/src/App.jsx
@@ -1,15 +1,32 @@
-
+import React from 'react';
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <div>
-      hello, ed
-    </div>
+      <div className="square_rows">
+        <Column/><Column/><Column/><Column/><Column/><Column/>
+      </div>
     </>
+
   )
 }
+
+function Square({ size = 50, color = "skyblue" }) {
+  return (
+    <div className = "square"> 
+          
+    </div>
+  );
+}
+function Column(){
+  return(
+    <div className="square_single_row">
+      <Square/><Square/><Square/><Square/><Square/>
+    </div>
+  );
+}
+
 
 export default App
