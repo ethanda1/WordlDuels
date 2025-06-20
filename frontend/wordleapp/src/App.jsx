3849d2bb1ca66b88127b34a3cb76e43f4a196b17
MysticalFlame <71807423+MysticalFlame@users.noreply.github.com>
2025-06-20 18:52:56 -0500

    User Input
    can type letters

--- frontend/wordleapp/src/App.jsx
+++ frontend/wordleapp/src/App.jsx
@@ -1,29 +1,78 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css'
 
+
 function App() {
+  const [round, setRound] = useState(1);
+  
+  const updateRound = () => {
+    setRound(round => round + 1)
+
+  }
 
   return (
     <>
-      <div className="square_rows">
-        <Column/><Column/><Column/><Column/><Column/><Column/>
+      <div>
+        <Row key={1} isActive = {1 === round} round= {updateRound}/>
+        <Row key={2} isActive = {2 === round} round= {updateRound}/>
+        <Row key={3} isActive = {3 === round} round= {updateRound}/>
+        <Row key={4} isActive = {4 === round} round= {updateRound}/>
+        <Row key={5} isActive = {5 === round} round= {updateRound}/>
+        <Row key={6} isActive = {6 === round} round= {updateRound}/>
       </div>
     </>
-
   )
 }
 
-function Square({ size = 50, color = "skyblue" }) {
+function Square({value}) {
+
   return (
-    <div className = "square"> 
-          
-    </div>
+    value.map((item) => (
+      <div className = "square">
+        {item}
+      </div>
+    ))
   );
 }
-function Column(){
+function Row({isActive ,round}){
+
+  const [value, setValue] = useState(['', '', '', '', '']);
+  const [index, setIndex] = useState(0); // current input index
+
+
+    useEffect(() => {
+      if (!isActive){
+        return;
+      }
+      const handleKeyDown = (e) => {
+        const letter = e.key.toUpperCase();
+        if (letter.match(/^[A-Z]$/) && index < 5) {
+          const newValue = [...value];
+          newValue[index] = letter;
+          setValue(newValue);
+          setIndex(index + 1);
+        }
+        if (e.key === 'Backspace' && index > 0) {
+          const newValue = [...value];
+          newValue[index - 1] = '';
+          setValue(newValue);
+          setIndex(index - 1);
+        }
+
+        if(e.key === 'Enter' && index === 5){
+          round();
+        }
+        console.log(e.key);
+      };
+
+      window.addEventListener('keydown', handleKeyDown);
+      return () => window.removeEventListener('keydown', handleKeyDown);
+    }, [value, index, isActive]);
+
+  
   return(
     <div className="square_single_row">
-      <Square/><Square/><Square/><Square/><Square/>
+      <Square value = {value}/>
     </div>
   );
 }
