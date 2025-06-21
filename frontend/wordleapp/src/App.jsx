0b09aa25fbccf0c7564191157c03f3f818337ac4
MysticalFlame <71807423+MysticalFlame@users.noreply.github.com>
2025-06-21 16:57:04 -0500

    Color stuff
    not complete

--- frontend/wordleapp/src/App.jsx
+++ frontend/wordleapp/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 
 function App() {
+  const word = 'Crane'
   const [round, setRound] = useState(1);
   
   const updateRound = () => {
@@ -13,12 +14,12 @@ function App() {
   return (
     <>
       <div>
-        <Row key={1} isActive = {1 === round} round= {updateRound}/>
-        <Row key={2} isActive = {2 === round} round= {updateRound}/>
-        <Row key={3} isActive = {3 === round} round= {updateRound}/>
-        <Row key={4} isActive = {4 === round} round= {updateRound}/>
-        <Row key={5} isActive = {5 === round} round= {updateRound}/>
-        <Row key={6} isActive = {6 === round} round= {updateRound}/>
+        <Row key={1} isActive = {1 === round} round= {updateRound} answer = {word}/>
+        <Row key={2} isActive = {2 === round} round= {updateRound} answer = {word}/>
+        <Row key={3} isActive = {3 === round} round= {updateRound} answer = {word}/>
+        <Row key={4} isActive = {4 === round} round= {updateRound} answer = {word}/>
+        <Row key={5} isActive = {5 === round} round= {updateRound} answer = {word}/>
+        <Row key={6} isActive = {6 === round} round= {updateRound} answer = {word}/>
       </div>
     </>
   )
@@ -34,11 +35,25 @@ function Square({value}) {
     ))
   );
 }
-function Row({isActive ,round}){
+function Row({isActive, round, answer}){
 
   const [value, setValue] = useState(['', '', '', '', '']);
   const [index, setIndex] = useState(0); // current input index
+  const answerArray= answer.toUpperCase().split('');
+  let answerLetterMap = {
+    //map contains indexs of each letter
+  };
 
+  const [colors, setColors] = useState([])
+  for (let i = 0; i < answer.length; i++) {
+    if(!(answerArray[i] in answerLetterMap)){
+      answerLetterMap[answerArray[i]] = [i];
+    }
+    else{
+      answerLetterMap[answerArray[i]].push(i);
+    }
+  }
+  const copy = answerLetterMap
 
     useEffect(() => {
       if (!isActive){
@@ -61,8 +76,21 @@ function Row({isActive ,round}){
 
         if(e.key === 'Enter' && index === 5){
           round();
+          for(let i=0; i<5; i++){
+            if(value[i] in answerLetterMap){ //checking for green and yellow
+              for(let j=0; j<answerLetterMap[value[i]].length; j++){
+                if(answerLetterMap[value[i]][j] === i){ //checking for green
+                  answerLetterMap[value[i]].splice(j,1); //removes green letter index from map
+                }
+                
+              }
+              //checks yellow 
+              answerLetterMap[value[i]].splice(0,1); //removes yellow letter
+            }
+          }
+          answerLetterMap = copy;
         }
-        console.log(e.key);
+        
       };
 
       window.addEventListener('keydown', handleKeyDown);
