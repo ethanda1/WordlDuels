19c3cfed5082f802818c5f8eb8d7d81704b8db9f
Ethan Dai <savii462@gmail.com>
2025-07-06 16:37:11 -0500

    http issue

--- frontend/wordleapp/src/App.jsx
+++ frontend/wordleapp/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   
 
   useEffect(() => {
-    const sock = new SockJS('http://3.149.252.70:80/echo');
+    const sock = new SockJS('https://3.149.252.70:80/echo');
     setSock(sock);
 
 
